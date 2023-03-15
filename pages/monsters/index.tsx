a0d1bb2ec50e653502a7c25264ea1f7ede2a3976
Ash CPC <jv.palha@gmail.com>
2023-03-15 18:11:11 -0300

    Add photos

--- pages/monsters/index.tsx
+++ pages/monsters/index.tsx
@@ -5,6 +5,7 @@ import { Header } from "../components/Header";
 import { useFetch } from "../hooks/useFetch";
 import styles from './styles.module.scss'
 import { useState } from "react";
+import {GiFishMonster} from 'react-icons/gi'
 
 interface Monster{
     name: string;
@@ -24,45 +25,8 @@ interface Type{
 
 export default function Monsters(){
 
-
-    const [sortCol, setSortCol] = useState<string>('name');
-    const [sortDir, setSortDir] = useState<'asc' | 'desc'>('asc');
     const [search, setSearch] = useState('')
-
     const {data, loading} = useFetch<Monster[]>('http://localhost:5000/api/monsters/query?nome='+search)
-    //--
-
-    function handleSort(colName: string) {
-      if (sortCol === colName) {
-        // Se a coluna já está selecionada, alterne a direção da ordenação
-        setSortDir(sortDir === 'asc' ? 'desc' : 'asc');
-      } else {
-        // Se a coluna ainda não está selecionada, selecione-a e ordene em ordem ascendente
-        setSortCol(colName);
-        setSortDir('asc');
-      }
-    }
-  
-    const sortedData = data
-      ? [...data].sort((a, b) => {
-        
-          const colA = a[sortCol as keyof Monster];
-          const colB = b[sortCol as keyof Monster];
-
-          if (typeof colA === 'string' && typeof colB === 'string') {
-            return sortDir === 'asc' ? colA.localeCompare(colB) : colB.localeCompare(colA);
-
-          } else if (typeof colA === 'string') {
-            return sortDir === 'asc' ? -1 : 1;
-
-          } else if (typeof colB === 'string') {
-            return sortDir === 'asc' ? 1 : -1;
-          } else{
-            return 0;
-          }
-        })
-      : [];
-      //--
 
     if(loading){
         return (
@@ -82,45 +46,38 @@ export default function Monsters(){
                 <input type="text" placeholder="Search Monster" onChange={(e)=>setSearch(e.target.value)}/>
             </form>
 
-            <table className={styles.table}>
-                <thead>
-                    <tr>
-                        <th onClick={() => handleSort('name')}>Name</th>
-                        <th onClick={() => handleSort('source')}>Source</th>
-                        <th onClick={() => handleSort('type')}>Type</th>
-                        <th onClick={() => handleSort('cr')}>CR</th>
-                    </tr>
-                </thead>
-                <tbody>
-            {sortedData?.map(monster => {
-                return (
-                        <tr>
-                            <td>
-                            <Link key={monster.name} href={{pathname: '/monster', query: {nome: monster.name, source: monster.source}}}>
-                                {monster.name}
-                            </Link>
-                            </td>
-                            <td>{monster.source}</td>
-                            <td>
-                            {monster.type
-                                ? typeof monster.type === 'string'
-                                ? monster.type
-                                : monster.type.type
-                                : ''}
-                            </td>
-                            <td>
-                            {monster.cr
-                                ? typeof monster.cr === 'string'
-                                ? monster.cr
-                                : monster.cr.cr
-                                : ''}
-                            </td>
-                            
-                        </tr>
-                )
-            })}
-                </tbody>
-            </table>
+            <div className={styles.container}>
+            {data?.map(monster => {
+                  const extensions = [".jpg", ".webp", ".png"];
+                  return (
+                    <div className={styles.box} key={monster.name}>
+                    {
+                        extensions.map((ext) => {
+                                return (
+                                    <img src={`http://localhost:5000/bestiary/${monster.source}/${monster.name}${ext}`} alt={monster.name}/>
+                                )
+                        })
+                    }
+                    <p>{monster.name}</p>
+                    <span>{monster.source}</span>
+                    <span>
+                        {monster.type
+                        ? typeof monster.type === 'string'
+                            ? monster.type
+                            : monster.type.type
+                        : ''}
+                    </span>
+                    <span>
+                        {monster.cr
+                        ? typeof monster.cr === 'string'
+                            ? monster.cr
+                            : monster.cr.cr
+                        : ''}
+                    </span>
+                    </div>
+                );
+                })}
+            </div>
         <Footer/>
         </>
     )
