738a23fd89ec808fa4bdae67d46d1e9908ff607b
Ash <jv.palha@gmail.com>
2023-03-17 14:04:25 -0300

    Setup monster unique page

--- pages/monster/index.tsx
+++ pages/monster/index.tsx
@@ -1,15 +1,35 @@
+import Head from "next/head"
 import { useRouter } from "next/router"
+import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
 import { useFetch } from "../hooks/useFetch"
+import styles from './styles.module.scss'
 export default function Monster(){
     const router = useRouter()
     const {nome, source} = router.query
     //API GET
-    //const {data, loading} = useFetch('http://localhost:5000/api/monsters/query?nome='+search)
+    const {data, loading} = useFetch(`http://localhost:5000/api/monsters/unique?nome=${nome}&source=${source}`, 1)
+    console.log(data)
+    if(loading){
+        return(
+            <>
+            <Header/>
+            <div>
+                <h1>Loading...</h1>
+            </div>
+            <Footer/>
+            </>
+        )
+    }
 
     return (
-        <>
-            <h1>Hello World from Monster</h1>
-            <p>{nome} -=-=- {source}</p>
+        <>  
+            <Header/>
+                <Head><title>{nome} - Monkeys & Dungeons</title></Head>
+                <main className={styles.main}>
+
+                </main>
+            <Footer/>
         </>
     )
 }
